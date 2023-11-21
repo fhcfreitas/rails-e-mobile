047c1d3aad5c98fd724b5c410a537bd5d18a1523
Fabio Freitas <fhcfreitas@gmail.com>
2023-11-21 18:08:30 -0300

    index route, controller and view html

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  # get 'products/index'
   devise_for :users
-  root to: "pages#home"
+  root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :products, only: [:index]
   # Defines the root path route ("/")
   # root "posts#index"
 end
