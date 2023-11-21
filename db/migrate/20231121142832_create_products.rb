7dff4f313da3b0dea7c71f75fa37fbd1e62714cf
Fabio Freitas <fhcfreitas@gmail.com>
2023-11-21 11:30:44 -0300

    User and products models

---
+++ db/migrate/20231121142832_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :model
+      t.float :price
+      t.string :location
+      t.string :category
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
