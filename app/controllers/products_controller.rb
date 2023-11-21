925cfa43455afd12e50fe844373683427ac2a68a
Joao Victor Saint Just <joaosaint@id.uff.br>
2023-11-21 15:43:04 -0300

    Description added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:model, :price, :location, :category)
+    params.require(:product).permit(:model, :description, :price, :location, :category)
   end
 end
