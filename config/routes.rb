4a7dc5dbf4a3f83346635b903550b4ee242e60ba
Joao Victor Saint Just <joaosaint@id.uff.br>
2023-11-22 16:49:49 -0300

    Added Edit and Delete to show and fixed issues

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,10 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-<<<<<<< HEAD
-  resources :products, only: [:index, :show, :edit, :update, :new, :create, :destroy]
-  resources :rentals, only: [:new, :create]
-=======
+  resources :products, only: [:destroy]
   resources :products, only: [:index, :show, :edit, :update, :new, :create] do
     resources :rentals, only: [:new, :create]
   end
->>>>>>> master
   # Defines the root path route ("/")
   # root "posts#index"
 end
