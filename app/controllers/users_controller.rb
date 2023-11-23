706d601c2ebd50f9ec841c06ca9d3fef96986b50
Nikolai Regehr <nikolaileonregehr@gmail.com>
2023-11-23 20:03:03 +0000

    footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
     @user = current_user
 
   end
+
+  def show
+    @user = User.find(params[:id])
+    @products_list = @user.rented_products.find(params[:id])
+  end
+
 end
